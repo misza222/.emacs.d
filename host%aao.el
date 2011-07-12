5c6d655eab4c03760e4ece0ccd157b1234b36ae2
Michal Pawlowski <misza222@gmail.com>
2011-07-12 13:14:11 +0100

    Renamed few files

---
+++ host%aao.el
@@ -0,0 +1,5 @@
+(setq scroll-step 10)
+
+(setq initial-frame-alist
+      '((left . 15) (top . 15)
+        (width . 170) (height . 40)))
--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;; should work on >= emacs.23
 
 ;; remove window features that are not necessary
-;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
@@ -12,16 +12,14 @@
 (add-to-list 'load-path dotfiles-dir)
 
 ;; load additional files
-(load "misza222-bindings")
-(load "misza222-misc")
-
+(load "bindings")
+(load "misc")
+(load "ruby")
 
 ;; Keep system or user specific customizations here
-(setq system-specific-config (concat dotfiles-dir system-name ".el")
-      user-specific-config (concat dotfiles-dir user-login-name ".el"))
-
+(setq system-specific-config (concat dotfiles-dir "host%" system-name ".el"))
 (if (file-exists-p system-specific-config) (load system-specific-config))
-(if (file-exists-p user-specific-config) (load user-specific-config))
+
 ;;; init.el ends here
 
 
