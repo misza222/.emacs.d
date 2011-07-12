638e6851c6c127ed9ceb376cbc9cf70b5ed97820
Michal Pawlowski <misza222@gmail.com>
2011-07-12 12:51:48 +0100

    Change file name according to name of the netbook

--- aao.el
+++ aao.el
@@ -3,10 +3,9 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 50 :width normal :foundry "unknown" :family "Monospace")))))
-(color-theme-zenburn)
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 70 :width normal :foundry "unknown" :family "Monospace")))))
 (setq scroll-step 3)
 
 (setq initial-frame-alist
       '((left . 10) (top . 5)
-        (width . 150) (height . 35)))
+        (width . 170) (height . 40)))
